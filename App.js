a89a8c51af608e3515be397b0ab0e831aaa66250
Andres <61568128+AndresGarciaU@users.noreply.github.com>
2020-03-06 00:51:57 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+import React,{useState} from 'react';
+import { StyleSheet, Text, View,TextInput, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{
+      
+      }}>
+    <View style={{
+      backgroundColor:'black',
+      
+      width:380,
+      height:900,
+      justifyContent:'center',
+      alignContent:'center'
+
+
+
+    }}>
+      <Button title="Galaga"
+      color='red'
+       />
+      
+      
+      <Button title="Start"
+       />
+      
+</View>
+<View/>
+</View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
