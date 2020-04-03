75072cf19610030038c075f316a47fbdc37e471b
Andres <afgarcia2@poligran.edu.co>
2020-04-03 01:34:28 -0500

    game engine+basic ship
    install of game engine,physics,a basic rectangle ship ,and constants ie the widht of the screen and height of the screen

--- Project-app/assets/Game.js
+++ Project-app/assets/Game.js
@@ -1,12 +1,44 @@
 import React,{ Component }  from 'react';
-import {Text} from 'react-native';
-
+import {Text, View, StyleSheet} from 'react-native';
+import {GameEngine} from 'react-native-game-engine';//game engine
+import Matter from 'matter-js'//physics engine
+import Constants from './Constants';
+import Ship from './Ship';
 export default class Game extends React.Component{
+    constructor(props){
+        super(props);
+        this.GameEngine=null;
+        this.entities=this.setUpWorld
+
+    }
+    setUpWorld = () => {
+         let engine =Matter.Engine.create({enableSleeping:false});
+         let world =engine.world;
+
+         let ship = Matter.Bodies.rectangle(Constants.MAX_WIDTH/4,Constants.MAX_HEIGHT/20,50,50);
+          Matter.world.add(world,[ship]);
+          return{
+              physics:{engine:engine,world:world},
+              Ship:{body:Ship,size:[50,50],color:'red',renderer:Ship}
+            }
+    }
+    
     render(){
         return(
-            <Text>
-                game page
-            </Text>
+            <View>
+               <GameEngine
+               ref={(ref)=>{this.GameEngine =ref;}}
+               style={styles.gameContainer}
+               entities={this.entities}
+               /> 
+                </View>
         );
     }
 }
+
+const styles=StyleSheet.create({
+ container:{
+     flex:1,
+     backgroundColor:'black'
+ }
+});
