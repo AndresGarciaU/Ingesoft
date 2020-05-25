d387bed37750598df195abe1aa979262fa98a4ff
Andres <afgarcia2@poligran.edu.co>
2020-05-25 18:01:22 -0500

    enemy
    enemy fire point+ enemy bullet + collision

---
+++ galaga/Assets/Bullet2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Bullet2 : MonoBehaviour
+{
+    public float Speed = 20f;
+    public int Damage = 1;
+    public Rigidbody2D Rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rb.velocity = transform.up * Speed;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Health player = hitInfo.GetComponent<Health>();
+        if (player != null)
+        {
+            player.TakeDamage(Damage);
+
+        }
+        Destroy(gameObject);
+    }
+
+
+}
---
+++ galaga/Assets/weapon2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon2 : MonoBehaviour
+{
+    public Transform FirePoint;
+    public GameObject bulletprafab;
+    
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.Range(0.0f,10.0f) <= 0.3)
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        Instantiate(bulletprafab, FirePoint.position, FirePoint.rotation);
+    }
+}
