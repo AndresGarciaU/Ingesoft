61e808ae331ff050dc93f345e83fadc228c79620
Andres <afgarcia2@poligran.edu.co>
2020-05-29 14:03:40 -0500

    puntuacion

--- galaga/Assets/Enemy.cs
+++ galaga/Assets/Enemy.cs
@@ -19,6 +19,7 @@ public class Enemy : MonoBehaviour
     }
     void Die()
     {
+        puntuacion.scoreValue += 10;
         Destroy(gameObject);
     }
     
--- galaga/Assets/puntuacion.cs
+++ galaga/Assets/puntuacion.cs
@@ -5,16 +5,17 @@ using UnityEngine.UI;
 
 public class puntuacion : MonoBehaviour
 {
+    public static int scoreValue = 0;
     public Text Puntuacion;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Puntuacion = GetComponent<Text>();   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Puntuacion.text = "Puntuacion:" + scoreValue; 
     }
 }
